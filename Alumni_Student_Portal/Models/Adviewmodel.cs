289a1719268f214e9d323d20def98480a13af889
Zaryab <sohail.zaryab61@gmail.com>
2020-04-14 04:09:59 -0700

    Ads view controller work

---
+++ Alumni_Student_Portal/Models/Adviewmodel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alumni_Student_Portal.Models
+{
+    public class Adviewmodel
+    {
+        public int Ad_id { get; set; }
+        public string Ad_name { get; set; }
+        public string Ad_image { get; set; }
+        public string Ad_des { get; set; }
+        public Nullable<int> pro_fk_Event_Category { get; set; }
+        public Nullable<int> pro_fk_Alumni { get; set; }
+
+
+        public int cat_id { get; set; }
+        public string cat_Name { get; set; }
+        public string cat_image { get; set; }
+    }
+}
