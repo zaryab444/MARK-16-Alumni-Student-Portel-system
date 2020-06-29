476303ecea31c0de9e8c7058b6883b089b95f0cf
Zaryab <sohail.zaryab61@gmail.com>
2020-06-29 00:01:54 -0700

    submit cv work

--- Alumni_Student_Portal/Controllers/AlumniController.cs
+++ Alumni_Student_Portal/Controllers/AlumniController.cs
@@ -14,7 +14,7 @@ namespace Alumni_Student_Portal.Controllers
         //
         // GET: /Alumni/
         Alumni_PortalEntities db = new Alumni_PortalEntities();
-        public ActionResult Index(int ? page)
+        public ActionResult Index(int? page)
         {
             int pagesize = 9, pageindex = 1;
             pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
@@ -30,22 +30,22 @@ namespace Alumni_Student_Portal.Controllers
         }
         [HttpPost]
         public ActionResult Registration(tbl_Alumni uvm)
-        {   
-                tbl_Alumni u = new tbl_Alumni();
-                u.Alumni_fullname = uvm.Alumni_fullname;
-                u.Alumni_Enrolmentnum = uvm.Alumni_Enrolmentnum;
-                u.Alumni_email = uvm.Alumni_email;
-                u.Alumni_cellnum = uvm.Alumni_cellnum;
-                u.Alumni_department = uvm.Alumni_department;
-                u.Alumni_cmsid = uvm.Alumni_cmsid;
-           
-                db.tbl_Alumni.Add(u);
-                db.SaveChanges();
-                return RedirectToAction("Login");
+        {
+            tbl_Alumni u = new tbl_Alumni();
+            u.Alumni_fullname = uvm.Alumni_fullname;
+            u.Alumni_Enrolmentnum = uvm.Alumni_Enrolmentnum;
+            u.Alumni_email = uvm.Alumni_email;
+            u.Alumni_cellnum = uvm.Alumni_cellnum;
+            u.Alumni_department = uvm.Alumni_department;
+            u.Alumni_cmsid = uvm.Alumni_cmsid;
 
-        
+            db.tbl_Alumni.Add(u);
+            db.SaveChanges();
+            return RedirectToAction("Login");
 
-         //   return View();
+
+
+            //   return View();
         }
         public ActionResult Login()
         {
@@ -111,9 +111,9 @@ namespace Alumni_Student_Portal.Controllers
             return path;
         }
 
-    
-    [HttpGet]
-    public ActionResult CreateAd()
+
+        [HttpGet]
+        public ActionResult CreateAd()
         {
             List<tbl_Event_Category> li = db.tbl_Event_Category.ToList();
             ViewBag.categorylist = new SelectList(li, "cat_id", "cat_name");
@@ -121,67 +121,156 @@ namespace Alumni_Student_Portal.Controllers
             return View();
         }
 
-[HttpPost]
-    public ActionResult CreateAd(Ad_Post pvm, HttpPostedFileBase imgfile)
-    {
-        List<tbl_Event_Category> li = db.tbl_Event_Category.ToList();
-        ViewBag.categorylist = new SelectList(li, "cat_id", "cat_name");
-
-
-        string path = uploadimgfile(imgfile);
-        if (path.Equals("-1"))
+        [HttpPost]
+        public ActionResult CreateAd(Ad_Post pvm, HttpPostedFileBase imgfile)
         {
-            ViewBag.error = "Image could not be uploaded....";
+            List<tbl_Event_Category> li = db.tbl_Event_Category.ToList();
+            ViewBag.categorylist = new SelectList(li, "cat_id", "cat_name");
+
+
+            string path = uploadimgfile(imgfile);
+            if (path.Equals("-1"))
+            {
+                ViewBag.error = "Image could not be uploaded....";
+            }
+            else
+            {
+                Ad_Post p = new Ad_Post();
+                p.Ad_name = pvm.Ad_name;
+                p.Ad_des = pvm.Ad_des;
+                p.Ad_image = path;
+                p.pro_fk_Event_Category = pvm.pro_fk_Event_Category;
+                // p.pro_fk_user = Convert.ToInt32(Session["u_id"].ToString());  // isko null krna ha
+                db.Ad_Post.Add(p);
+                db.SaveChanges();
+                Response.Redirect("Index");
+
+            }
+
+            return View();
         }
-        else
+
+        public ActionResult Ads(int? id, int? page)
         {
-            Ad_Post p = new Ad_Post();
-            p.Ad_name = pvm.Ad_name;
-            p.Ad_des = pvm.Ad_des;
-            p.Ad_image = path;
-            p.pro_fk_Event_Category = pvm.pro_fk_Event_Category;
-            // p.pro_fk_user = Convert.ToInt32(Session["u_id"].ToString());  // isko null krna ha
-            db.Ad_Post.Add(p);
-            db.SaveChanges();
-            Response.Redirect("Index");
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var list = db.Ad_Post.Where(x => x.pro_fk_Event_Category == id).OrderByDescending(x => x.Ad_id).ToList();
+            IPagedList<Ad_Post> stu = list.ToPagedList(pageindex, pagesize);
+
+
+            return View(stu);
+
 
         }
-
-        return View();
-    }
-
-public ActionResult Ads(int? id, int? page)
-{
-    int pagesize = 9, pageindex = 1;
-    pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
-    var list = db.Ad_Post.Where(x => x.pro_fk_Event_Category == id).OrderByDescending(x => x.Ad_id).ToList();
-    IPagedList<Ad_Post> stu = list.ToPagedList(pageindex, pagesize);
-
-
-    return View(stu);
-
-
-}
-public ActionResult ViewAd(int? id)
-{
-    Adviewmodel ad = new Adviewmodel();
-    Ad_Post p = db.Ad_Post.Where(x => x.Ad_id == id).SingleOrDefault();
-    ad.Ad_id = p.Ad_id;
-    ad.Ad_name = p.Ad_name;
-    ad.Ad_image = p.Ad_image;
-    ad.Ad_des = p.Ad_des;
-    tbl_Event_Category cat = db.tbl_Event_Category.Where(x => x.cat_id == p.pro_fk_Event_Category).SingleOrDefault();
-    ad.cat_Name = cat.cat_Name;
-    //tbl_user u = db.tbl_user.Where(x => x.u_id == p.pro_fk_user).SingleOrDefault();
-    //ad.u_name = u.u_name;
-    //ad.cat_image = u.cat_image;
-    //ad.u_contact = u.u_contact;
-    //ad.pro_fk_user = u.u_id;
+        public ActionResult ViewAd(int? id)
+        {
+            Adviewmodel ad = new Adviewmodel();
+            Ad_Post p = db.Ad_Post.Where(x => x.Ad_id == id).SingleOrDefault();
+            ad.Ad_id = p.Ad_id;
+            ad.Ad_name = p.Ad_name;
+            ad.Ad_image = p.Ad_image;
+            ad.Ad_des = p.Ad_des;
+            tbl_Event_Category cat = db.tbl_Event_Category.Where(x => x.cat_id == p.pro_fk_Event_Category).SingleOrDefault();
+            ad.cat_Name = cat.cat_Name;
+            //tbl_user u = db.tbl_user.Where(x => x.u_id == p.pro_fk_user).SingleOrDefault();
+            //ad.u_name = u.u_name;
+            //ad.cat_image = u.cat_image;
+            //ad.u_contact = u.u_contact;
+            //ad.pro_fk_user = u.u_id;
 
 
 
 
-    return View(ad);
-}
+            return View(ad);
+        }
+        public ActionResult SubmitCV()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitCV(CV C, HttpPostedFileBase file)
+        {
+            string fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);//com   
+            C.cv1 = (Path.Combine(Server.MapPath("~/uploadcv"), fileName));//com
+            string path = uploadfile(file);
+            if (path.Equals("-1"))
+            {
+                ViewBag.error = "file could not be uploaded....";
+            }
+            else
+            {
+
+                CV u = new CV();
+                u.Cid = C.Cid;
+                u.fullname = C.fullname;
+                u.Email = C.Email;
+                u.pro_fk_Alumni = u.pro_fk_Alumni;
+                u.cv1 = path;
+
+                db.CVs.Add(u);//com
+                //db.SaveChanges();//com
+                string filename = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                C.cv1 = (Path.Combine(Server.MapPath("~/Uploadcv"), filename));
+                C.cv1 = Path.Combine(Server.MapPath("~/Uploadcv"), filename);
+
+                using (Alumni_PortalEntities dc = new Alumni_PortalEntities())
+                {
+                    C.cv1 = filename;
+                    dc.CVs.Add(C);
+                    dc.SaveChanges(); //com
+
+
+
+
+
+
+
+                }//
+
+            }
+            return View();
+        }
+        public string uploadfile(HttpPostedFileBase file)
+        {
+            Random r = new Random();
+            string path = "-1";
+            int random = r.Next();
+            if (file != null && file.ContentLength > 0)
+            {
+                string extension = Path.GetExtension(file.FileName);
+                if (extension.ToLower().Equals(".pdf") || extension.ToLower().Equals(".docs"))
+                {
+                    try
+                    {
+
+                        path = Path.Combine(Server.MapPath("~/UploadCV"), random + Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+                        path = "~/UploadCV" + random + Path.GetFileName(file.FileName);
+
+                        ViewBag.Message = "File uploaded successfully";
+                    }
+                    catch (Exception ex)
+                    {
+                        path = "-1";
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('Only pdf formats are acceptable....'); </script>");
+                }
+            }
+
+            else
+            {
+                Response.Write("<script>alert('Please select a file'); </script>");
+                path = "-1";
+            }
+
+
+
+            return path;
+        }
+
     }
 }
